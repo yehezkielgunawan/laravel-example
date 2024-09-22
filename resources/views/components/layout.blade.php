4d38d81aa80978b7cdf158d7fedcd1aa72ce9f3d
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-09-22 13:33:28 +0700

    feat: :sparkles: blade templating

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
+    <title>Home Page</title>
 </head>
 
 <body>
-    <h1>Hello from About Page.</h1>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+
+    {{ $slot }}
 </body>
 
 </html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1>Hello from home Page</h1>
+</x-layout>
